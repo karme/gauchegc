bc017e19c5b03f87adcc8346e02ad69a3cef3ba6
Jens Thiele <karme@karme.de>
2012-10-09 15:41:15 +0200

    API change: make optionals keywords and work around string output port troubles

--- svg-plot/test.scm
+++ svg-plot/test.scm
@@ -16,7 +16,7 @@
    (cut svg-plot
         '(((0 0) (10 10) (20 5) (30 30))
           ((0 0) (10 9) (20 4) (30 20)))
-        '("test1" "test2")))
+        :titles '("test1" "test2")))
   (with-output-to-process
    `(see ,#`"image/svg+xml:-")
    (cut svg-plot-3d
@@ -26,5 +26,10 @@
           (((0 0 0.2) (1 0 0.0) (2 0 -0.5))
            ((0 1 0.0) (1 1 0.4) (2 1  0.0))
            ((0 2 0.0) (1 2 0.0) (2 2  0.3))))
-        '("test1" "test")))
+        :titles '("test1" "test")
+        :size '(1024 768)))
+  #?=(string? (with-output-to-string
+                (cut svg-plot
+                     '(((0 0) (10 10) (20 5) (30 30))
+                       ((0 0) (10 9) (20 4) (30 20))))))
   0)
