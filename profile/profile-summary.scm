22e0bebe9f6fa4306fa0dbea3a318470c513e174
Jens Thiele <karme@karme.de>
2012-11-20 17:39:15 +0100

    some more stats, SVG output

--- profile/profile-summary.scm
+++ profile/profile-summary.scm
@@ -17,10 +17,35 @@
 (define unique queue-unique)
 
 (define clear-terminal
-  (let1 s (process-output->string `(tput clear))
-    (lambda()
-      (display s)
-      (flush))))
+  (guard [e
+          [else
+           (lambda _)]]
+         (let1 s (process-output->string `(tput clear))
+           (lambda()
+             (display s)
+             (flush)))))
+
+(define (runtime->string x)
+  #`",(ceiling->exact (* x 1000))ms")
+
+(define (stats-by-source all)
+  (sort (filter boolean
+                (map (lambda(l)
+                       (let1 times (filter boolean
+                                           (map (lambda(x) (car (assoc-ref x 'runtime '(#f)))) l))
+                         (if (null? times)
+                           #f
+                           `((source ,(car (assoc-ref (car l) 'source)))
+                             (count ,(size-of l))
+                             (total-time ,(apply + times))
+                             (min-time ,(apply min times))
+                             (max-time ,(apply max times))
+                             (avg-time ,(apply avg times))))))
+                     (group-collection all
+                                       :key (lambda(x) (assoc-ref x 'source))
+                                       :test equal?)))
+        (lambda(x y) (> (car (assoc-ref x 'total-time))
+                        (car (assoc-ref y 'total-time))))))
 
 (define (print-totals all)
   (for-each (lambda(x)
@@ -29,22 +54,7 @@
                        "unknown: ")
                      (filter (lambda(x) (not (equal? (car x) 'source)))
                              x)))
-            (sort (filter boolean (map (lambda(l)
-                                         (let1 times (filter boolean
-                                                             (map (lambda(x) (car (assoc-ref x 'runtime '(#f)))) l))
-                                           (if (null? times)
-                                             #f
-                                             `((source ,(car (assoc-ref (car l) 'source)))
-                                               (count ,(size-of l))
-                                               (total-time ,(apply + times))
-                                               (min-time ,(apply min times))
-                                               (max-time ,(apply max times))
-                                               (avg-time ,(apply avg times))))))
-                                       (group-collection all
-                                                         :key (lambda(x) (assoc-ref x 'source))
-                                                         :test equal?)))
-                  (lambda(x y) (> (car (assoc-ref x 'total-time))
-                                  (car (assoc-ref y 'total-time)))))))
+            (stats-by-source all)))
 
 (define (callgraph-edges all)
   (group-collection (map (lambda(x)
@@ -63,30 +73,61 @@
                     :test equal?))
 
 (define (print-graph all)
-  (let ((node-name (lambda(n)
-                     (if (car n)
-                       (string-append (caar n) ":" (x->string (cadar n)))
-                       "unknown" ;; todo
-                       ))))
+  (let* ((node-name (lambda(n)
+                      (if (car n)
+                        (string-append (caar n) ":" (x->string (cadar n)))
+                        "unknown" ;; todo
+                        )))
+         (all-node-stats (stats-by-source all))
+         (node-stats (lambda(n)
+                       (and (car n)
+                            (find (lambda(x)
+                                    (equal? (car (assoc-ref x 'source))
+                                            (car n)))
+                                  all-node-stats))))
+         (node-time (lambda(n)
+                      (if-let1 ns (node-stats n)
+                        (car (assoc-ref ns 'total-time '(#f)))
+                        #f)))
+         (node->string (lambda(n)
+                         (string-append "[ " (node-name n)
+                                        " "
+                                        (if-let1 ns (node-stats n)
+                                          (string-append "("
+                                                         (string-join
+                                                          (list
+                                                           #`"cnt=,(car (assoc-ref ns 'count))"
+                                                           #`"t=,(runtime->string (car (assoc-ref ns 'total-time)))")
+                                                          ",")
+                                                         ")")
+                                          "" ; todo
+                                          )
+                                        " ]"))))
     (with-output-to-process
-     '(graph-easy --as boxart)
+     '(sh -c "graph-easy --as dot|dot -Tsvg")
+     ;;'(cat)
      (lambda()
        (print "graph{flow:south}")
        (for-each (lambda(edges)
-                   (print "[ " (node-name (car (assoc-ref (car edges) 'source))) " ]"
-                          " - "
-                          (apply + (map (lambda(e)
-                                          (car (assoc-ref e 'runtime)))
-                                        edges))
-                          " -> "
-                          "[ " (node-name (car (assoc-ref (car edges) 'target))) " ]"))
+                   (let ((et (apply + (map (lambda(e)
+                                             (car (assoc-ref e 'runtime)))
+                                           edges))))
+                     (print (node->string (car (assoc-ref (car edges) 'source)))
+                            " --> { label: \" " ;; note: additional whitespace to make graphviz happy
+                            ;; todo: escape
+                            (runtime->string et)
+                            (if-let1 nt (node-time (car (assoc-ref (car edges) 'source)))
+                              #`"(,(round->exact (* (/ et nt) 100))%)"
+                              "")
+                            "\"; } "
+                            (node->string (car (assoc-ref (car edges) 'target))))))
                  (callgraph-edges all)))
      :output (current-output-port))))
 
 (define (main args)
   (let ((all '())
         (start-time (current-time))
-        (update-interval 1))
+        (update-interval 2))
     (while (read) (compose not eof-object?) => l
            (push! all l)
            (let1 end-time (current-time)
@@ -95,5 +136,8 @@
                (clear-terminal)
                ;; todo: might be slower than update-interval ;-)
                (print-totals all)
-               (print-graph all)))))
+               (print-graph all)
+               )))
+    (print-totals all)
+    (print-graph all))
   0)
