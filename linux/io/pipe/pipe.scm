14be9443ddaef18fd6767222e1ca855a71ab2fc1
Jens Thiele <karme@karme.de>
2015-11-21 21:55:16 +0100

    add c-vmsplice

--- linux/io/pipe/pipe.scm
+++ linux/io/pipe/pipe.scm
@@ -3,15 +3,19 @@
   (use gauche.uvector)
   (export set-pipe-buffer-size!
           get-pipe-buffer-size
-          vmsplice-block))
+          vmsplice-block
+          c-vmsplice))
 
 (select-module pipe)
 (c-load "fcntl.h" :cppflags "-D_GNU_SOURCE")
+(c-load '("stdint.h" "errno.h"))
+
+(define (get-errno)
+  ;; todo: i am not sure how errno is handled by gauche/c-wrapper in different versions
+  (cast <number> (deref (__errno_location))))
 
 (define (my-fcntl port . l)
   (apply fcntl (cons (port-file-number port) l)))
-
-
 
 ;; todo: F_GETPIPE_SZ (void; since Linux 2.6.35)
 (define (get-pipe-buffer-size p)
@@ -37,7 +41,7 @@
                 (c-ptr+ (cast (ptr <c-void>) b) offset))
           ;; todo
           (when (not (zero? (modulo (cast <number>
-                                          (cast <uint>
+                                          (cast <uintptr_t>
                                                 (ref iov 'iov_base)))
                                     4096)))
             #?="maybe not page alligned?")
@@ -45,4 +49,23 @@
           (let1 r (vmsplice (port-file-number (current-output-port)) (ptr iov) 1 0)
             (when (<= r 0)
               (error "splice failed"))
+            (set! offset (+ offset r))))))))
+
+(define c-vmsplice
+  (let1 iov (make <c-struct:iovec>)
+    (lambda(p s)
+      (let ((offset 0))
+        (while (< offset s)
+          (set! (ref iov 'iov_base)
+                (c-ptr+ p offset))
+          ;; todo
+          (when (not (zero? (modulo (cast <number>
+                                          (cast <uintptr_t>
+                                                (ref iov 'iov_base)))
+                                    4096)))
+            #?="maybe not page alligned?")
+          (set! (ref iov 'iov_len) (- s offset))
+          (let1 r (vmsplice (port-file-number (current-output-port)) (ptr iov) 1 0)
+            (when (<= r 0)
+              (error "splice failed: " (get-errno)))
             (set! offset (+ offset r))))))))
