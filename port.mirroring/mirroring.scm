51f03bab46c894347ce4eb8e06db6a1c7bae35eb
Shigenobu Kimura <skimu@me.com>
2016-02-06 16:20:32 -0600

    new version

--- port.mirroring/mirroring.scm
+++ port.mirroring/mirroring.scm
@@ -21,42 +21,62 @@
    4))
 |#
 
-(define (open-mirroring-input port dest-port)
-  (let ((mirror-port (make <virtual-input-port>)))
+(define (open-mirroring-input in dest-port)
+  (let ((mi (make <virtual-input-port>)))
     (define (getb)
-      (let ((c (read-byte port)))
-        (if (eof-object? c)
-            c
-            (begin
-              (write-byte c dest-port)
-              c))))
+      (let ((b (read-byte in)))
+        (cond ((eof-object? b) b)
+              (else
+               (write-byte b dest-port)
+               b))))
     (define (getc)
-      (let ((c (read-char port)))
-        (if (eof-object? c)
-            c
-            (begin
-              (display c dest-port)
-              c))))
+      (let ((c (read-char in)))
+        (cond ((eof-object? c) c)
+              (else
+               (write-char c dest-port)
+               c))))
     (define (gets n)
-      (let ((str (read-block n port)))
-        (if (eof-object? str)
-            str
-            (begin
-              (display str dest-port)
-              str))))
+      (let ((str (read-block n in)))
+        (cond ((eof-object? str) str)
+              (else
+               (display str dest-port)
+               str))))
     (define (ready char?)
       (if char?
-          (char-ready? port)
-          (byte-ready? port)))
+        (char-ready? in)
+        (byte-ready? in)))
     (define (seek offset whence)
-      (port-seek port offset whence))
+      (port-seek in offset whence))
+    (slot-set! mi 'getb getb)
+    (slot-set! mi 'getc getc)
+    (slot-set! mi 'gets gets)
+    (slot-set! mi 'ready ready)
+    (slot-set! mi 'seek seek)
+    mi))
 
-    (slot-set! mirror-port 'getb getb)
-    (slot-set! mirror-port 'getc getc)
-    (slot-set! mirror-port 'gets gets)
-    (slot-set! mirror-port 'ready ready)
-    (slot-set! mirror-port 'seek seek)
-    mirror-port))
+(define (open-mirroring-output out dest-port)
+  (let ((mo (make <virtual-output-port>)))
+    (define (putb b)
+      (write-byte b dest-port)
+      (write-byte b out))
+    (define (putc c)
+      (write-char c dest-port)
+      (write-char c out))
+    (define (puts str)
+      (display str dest-port)
+      (display str out))
+    (define (flus)
+      (flush dest-port)
+      (flush out))
+    (define (seek offset whence)
+      (port-seek out offset whence))
+    (slot-set! mo 'putb  putb)
+    (slot-set! mo 'putc  putc)
+    (slot-set! mo 'puts  puts)
+    (slot-set! mo 'flush flus)
+    (slot-set! mo 'seek  seek)
+    mo))
+
 
 (define (call-with-mirroring-input port dest-port proc)
   (let1 p (open-mirroring-input port dest-port)
@@ -65,39 +85,47 @@
         (close-port p)))))
 
 (define (with-input-from-port/mirroring-to-port port dest-port thunk)
-  (call-with-mirroring-input port
-			     dest-port
-			     (lambda (p)
-			       (with-input-from-port p thunk))))
+  (call-with-mirroring-input
+      port dest-port
+    (lambda (p) (with-input-from-port p thunk))))
 
 (define (with-input-from-port/mirroring-to-file port dest-file thunk)
   (call-with-output-file dest-file
     (lambda (dest-port)
-      (with-input-from-port/mirroring-to-port port
-                                              dest-port
-                                              thunk))))
+      (with-input-from-port/mirroring-to-port
+          port dest-port
+        thunk))))
 
 (define (with-input-from-file/mirroring-to-port file dest-port thunk)
   (call-with-input-file file
     (lambda (port)
-      (with-input-from-port/mirroring-to-port port
-                                              dest-port
-                                              thunk))))
+      (with-input-from-port/mirroring-to-port
+          port dest-port
+        thunk))))
 
 (define (with-input-from-file/mirroring-to-file file dest-file thunk)
   (call-with-input-file file
     (lambda (port)
       (call-with-output-file dest-file
         (lambda (dest-port)
-          (with-input-from-port/mirroring-to-port port
-                                                  dest-port
-                                                  thunk))))))
+          (with-input-from-port/mirroring-to-port
+              port dest-port
+            thunk))))))
 
 (define (with-input-from-string/mirroring-to-port str dest-port thunk)
   (call-with-input-string str
     (lambda (port)
-      (with-input-from-port/mirroring-to-port port
-                                              dest-port
-                                              thunk))))
+      (with-input-from-port/mirroring-to-port
+          port dest-port
+        thunk))))
+
+(define (with-input-from-string/mirroring-to-file str dest-file thunk)
+  (call-with-input-string str
+    (lambda (port)
+      (call-with-output-file dest-file
+        (lambda (dest-port)
+          (with-input-from-port/mirroring-to-port
+              port dest-port
+            thunk))))))
 
 (provide "ggc/port/mirroring")
--- port.mirroring/test.scm
+++ port.mirroring/test.scm
@@ -1,5 +1,6 @@
 (use gauche.test)
 (test-start "ggc.port.mirroring")
+
 (use ggc.port.mirroring)
 (test-module 'ggc.port.mirroring)
 
@@ -15,26 +16,42 @@
        (with-output-to-string
          (lambda ()
            (with-input-from-string/mirroring-to-port
-            "abracadabra" (current-output-port)
-            (lambda ()
-              (read-char)
-              (read-char)
-              )))))
+               "abracadabra" (current-output-port)
+             (lambda ()
+               (read-char)
+               (read-char)
+               )))))
 
-(test* "check if port is closed on error" #t
+(test* "check if port gets closed on error" #t
        (let ((port #f))
          (guard (e (else (and port (port-closed? port))))
+           (with-input-from-string/mirroring-to-file
+               "abracadabra" "fo.txt"
+             (lambda ()
+               (set! port (current-input-port))
+               (read-char)
+               (error "for test"))))))
+
+(test* "port-name returns #f" "#f\n"
+       (with-output-to-string
+         (lambda ()
            (with-input-from-string/mirroring-to-port
-            "abracadabra" (current-output-port)
-            (lambda ()
-              (set! port (current-input-port))
-              (read-char)
-              (error "for test"))))))
+               "abracadabra" (current-output-port)
+             (lambda ()
+               (print (port-name (current-input-port))))))))
+
+(test* "port-current-line works fine" "\n\n3\n"
+       (with-output-to-string
+         (lambda ()
+           (with-input-from-string/mirroring-to-port
+               "\n\nabracadabra" (current-output-port)
+             (lambda ()
+               (read-char)              ; outs #\nl
+               (read-char)              ; outs #\nl
+               (print (port-current-line (current-input-port))) ; outs #\3 #\nl
+               )))))
 
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
 ;; EOF
-
-                     
-                  
