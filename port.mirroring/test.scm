4392c9f13922f31f24161ba737488f3ebfd3dcc6
Shigenobu Kimura <skimu@me.com>
2016-02-06 16:38:23 -0600

    add test-script

--- port.mirroring/test.scm
+++ port.mirroring/test.scm
@@ -51,6 +51,8 @@
                (print (port-current-line (current-input-port))) ; outs #\3 #\nl
                )))))
 
+(test-script "./gpsh")
+
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
 (test-end :exit-on-failure #t)
